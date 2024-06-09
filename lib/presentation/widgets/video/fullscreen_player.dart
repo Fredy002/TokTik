9662806734712b2678c25b91516c4f048718974d
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-09 17:48:30 -0500

    Refactor FullscreenPlayer to support video captions

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullscreenPlayer extends StatefulWidget {
@@ -44,14 +45,63 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
   @override
   Widget build(BuildContext context) {
     return controller.value.isInitialized
-        ? AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: VideoPlayer(controller),
+        ? GestureDetector(
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+                return;
+              }
+              controller.play();
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(children: [
+                VideoPlayer(
+                  controller,
+                ),
+
+                // gradient
+                VideoBackground(
+                  stops: const [
+                    0.8,
+                    1.0,
+                  ],
+                ),
+
+                //text
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(caption: widget.caption),
+                )
+              ]),
+            ),
           )
         : const Center(
             child: CircularProgressIndicator(
               strokeWidth: 2,
             ),
           );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+
+  const _VideoCaption({required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
+    );
   }
 }
