22b72d593eff768040ee00e3e6f44a78a9857fc0
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-09 17:17:34 -0500

    feat: Improve lazy loading for images in DiscoverProviderPexels

--- lib/presentation/providers/discover_provider_pexels.dart
+++ lib/presentation/providers/discover_provider_pexels.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/infrastructure/models/pexels_video_model.dart';
@@ -8,12 +10,25 @@ class DiscoverProviderPexels extends ChangeNotifier {
   List<VideoPost> videos = [];
   final PexelsAPI pexelsAPI = PexelsAPI();
 
+  DiscoverProviderPexels() {
+    loadNextPage();
+  }
+
   Future<void> loadNextPage() async {
     try {
       final List<PexelsVideoModel> newVideos =
           await pexelsAPI.fetchVideos(query: 'nature', page: 1, perPage: 10);
-      videos
-          .addAll(newVideos.map((video) => video.toVideoPostEntity()).toList());
+
+      final random = Random();
+      videos.addAll(
+        newVideos.map(
+          (video) => video.toVideoPostEntity(
+            likes: random.nextInt(1000) + 1000,
+            views: random.nextInt(1000) + 1000,
+          ),
+        ),
+      );
+
       initialLoading = false;
       notifyListeners();
     } catch (e) {
